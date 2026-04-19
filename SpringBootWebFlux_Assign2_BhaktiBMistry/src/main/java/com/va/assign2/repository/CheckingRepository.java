f8f6e27fd3bfb04656a4fca19378fdb58c5f20a0
BhaktiMistry <134191230+BhaktiMistry@users.noreply.github.com>
2026-04-19 16:35:06 -0400

    Practical Test-2 added

--- SpringBootWebFlux_Assign2_BhaktiBMistry/src/main/java/com/va/assign2/repository/CheckingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.va.assign2.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-import com.va.assign2.model.Checking;
-
-@Repository
-public interface CheckingRepository extends ReactiveMongoRepository<Checking, String> { }
--- spring-boot-ribbon/ribbon-client/src/main/java/com/example/ribbonclient/RibbonConfiguration.java
+++
@@ -1,4 +0,0 @@
-package com.example.ribbonclient;
-
-public class RibbonConfiguration {
-}

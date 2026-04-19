63b51ea82f640157eb1691e237fdf4244575ca62
BhaktiMistry <134191230+BhaktiMistry@users.noreply.github.com>
2026-04-19 16:55:42 -0400

    Practical Test-2 added

---
+++ SpringBootWebFlux_Assign2_BhaktiBMistry/src/main/java/com/va/assign2/model/Savings.java
@@ -0,0 +1,24 @@
+package com.va.assign2.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Document(collection = "savings")
+public class Savings extends Account {
+    private double interestRate;
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+}
